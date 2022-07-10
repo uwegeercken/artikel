ccda67d8ddc82f5573f2b6a86604ceadad03b1b5
uwegeercken <uwegeercken@web.de>
2022-07-10 14:50:33 +0200

    added some comments

--- config/run-artikel-app.sh
+++ config/run-artikel-app.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
+#
+# script to run the artikel web application.
+#
+# replace the workingDir variable according to your setup.
+# replace the <version> tag below with the version you have.
+#
 
 workingDir=/opt/artikel
-applicationDir=${workingDir}/application
+applicationDir=${workingDir}/artikel<version>
 
 java -jar ${applicationDir}/artikel.jar ${workingDir}/config.yaml
-
-
-
 
