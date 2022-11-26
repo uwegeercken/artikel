3b5efc746f2222ef88f10930a3cf02451a51e5d2
uwegeercken <uwegeercken@web.de>
2022-11-26 17:24:33 +0100

    upgraded version

--- src/main/java/com/datamelt/artikel/app/web/WebApplication.java
+++ src/main/java/com/datamelt/artikel/app/web/WebApplication.java
@@ -28,8 +28,8 @@ public class WebApplication
 {
     private static final Logger logger =  LoggerFactory.getLogger(WebApplication.class);
 
-    public static final String APPLCATION_VERSION = "v2.6";
-    public static final String APPLCATION_LAST_UPDATE = "09.11.2022";
+    public static final String APPLCATION_VERSION = "v2.7";
+    public static final String APPLCATION_LAST_UPDATE = "26.11.2022";
 
     private static SecretKey secretKey = null;
     private static MessageBundleInterface messages;
